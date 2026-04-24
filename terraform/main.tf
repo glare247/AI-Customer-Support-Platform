8868ac911d0bfe3c1ff2d476891fd50aaa91b53a
Grace Obetoh <awsproject92@gmail.com>
2026-04-24 21:50:18 +0100

    Update EKS node group to t3.small and finalize infra setup

--- terraform/main.tf
+++ terraform/main.tf
@@ -186,8 +186,8 @@ resource "aws_eks_node_group" "nodes" {
   ]
 
   scaling_config {
-    desired_size = 2
-    max_size     = 3
+    desired_size = 1
+    max_size     = 1
     min_size     = 1
   }
 
